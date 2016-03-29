3ee1572e053612f048d5f401112037d5f573d421
Haider Malik <haidermalik5044@gmail.com>
2016-03-30 02:25:27 +0500

    Add Mongodb and fecth the ContactList Data from Mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactList', ['contactList']);
 
 // use html and css static files
 app.use(express.static(__dirname + '/public'));
@@ -10,26 +12,13 @@ app.get('/contactList', function(req, res) {
 
     console.log('I received a GET request');
 
-    var person = {
-        name: 'Haider',
-        email: 'haidermalik504@gmail.com',
-        number: '(112)111-111'
-    };
+    db.contactList.find(function(err, docs) {
+        console.log(docs);
+        // send response to Controller with contact List Data
+        res.json(docs);
+    });
 
-    var person2 = {
-        name: 'Faham',
-        email: 'faham@gmail.com',
-        number: '(333)333-333'
-    };
-    var person3 = {
-        name: 'Mohsin',
-        email: 'Mohsin@gmail.com',
-        number: '3043-2222-211'
-    }
 
-    var contactList = [person, person2, person3];
-    // send response to Controller with contact List Data
-    res.json(contactList);
 });
 
 app.listen(3000);
