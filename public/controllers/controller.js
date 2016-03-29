fc05b6269499556b56a6230ce03f5c0952f7f155
Haider Malik <haidermalik5044@gmail.com>
2016-03-30 00:43:20 +0500

    Add Dummy Data to ContactList App

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,24 @@
 var app = angular.module('contactApp', []);
+var person = {
+    name: 'Haider',
+    email: 'haidermalik504@gmail.com',
+    number: '(112)111-111'
+};
+
+var person2 = {
+    name: 'Faham',
+    email: 'faham@gmail.com',
+    number: '(333)333-333'
+};
+var person3 = {
+    name: 'Mohsin',
+    email: 'Mohsin@gmail.com',
+    number: '3043-2222-211'
+}
+
+var contactList = [person, person2, person3];
+
 app.controller('AppCtrl', function($scope) {
-    console.log('Hello From controller');
+
+    $scope.contactList = contactList;
 });
